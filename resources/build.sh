e362047241a6f8922d5364de8f91113b755186a3
Vishal Menon <vishalmenon.92@gmail.com>
2021-11-13 14:30:29 +0530

    Updated build.sh

--- resources/build.sh
+++ resources/build.sh
@@ -4,15 +4,18 @@
 # NOTE: THIS SCRIPT MUST BE RUN FROM WITHIN THIS DIRECTORY.
 # NOTE: AVOID USING RELATIVE PATH TO CHANGE THIS. USE CARGO MANIFEST DIR ENV VARIABLE?
 #
+export ANDROID_NDK_HOME=$HOME/Android/Sdk/ndk
+
+AARCH64_LINKER=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/darwin-x86_64/bin/aarch64-linux-android29-clang
+I686_LINKER=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/darwin-x86_64/bin/i686-linux-android29-clang
+X86_64_LINKER=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/darwin-x86_64/bin/x86_64-linux-android29-clang
+
 rm -rf ../builds*
 # rm -rf ../target
 # cargo clean
-cargo install cargo-add
-cargo add openssl
+# cargo install cargo-add
+# cargo add openssl
 
-# AARCH64_LINKER=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/darwin-x86_64/bin/aarch64-linux-android29-clang
-# I686_LINKER=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/darwin-x86_64/bin/i686-linux-android29-clang
-# X86_64_LINKER=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/darwin-x86_64/bin/x86_64-linux-android29-clang
 
 mkdir -p ../builds/x86_64-linux-android/release/
 mkdir -p ../builds/aarch64-linux-android/release/
@@ -21,9 +24,9 @@ mkdir -p ../builds/i686-linux-android/release/
 # mkdir -p ../builds/x86_64-apple-darwin/release/
 
 # rustup target add x86_64-apple-darwin
-rustup target add aarch64-linux-android
-rustup target add x86_64-linux-android
-rustup target add i686-linux-android
+# rustup target add aarch64-linux-android
+# rustup target add x86_64-linux-android
+# rustup target add i686-linux-android
 
 
 # [ ! -d osxcross ] && bash $(pwd)/osxcross.sh
@@ -41,16 +44,19 @@ rustup target add i686-linux-android
 # cargo build --release --target x86_64-apple-darwin
 
 # android
-cargo build --release --target aarch64-linux-android
-cargo build --release --target x86_64-linux-android
+# cargo build --release --target aarch64-linux-android
+# cargo build --release --target x86_64-linux-android
 # cargo build --release --target i686-linux-android
 
-cp ../target/aarch64-linux-android/release/libstackmate.d ../builds/aarch64-linux-android/release/
-cp ../target/x86_64-linux-android/release/libstackmate.a ../builds/x86_64-linux-android/release/
-# cp ../target/i686-linux-android/release/libstackmate.so ../builds/i686-linux-android/release/
 
-strip ../builds/aarch64-linux-android/release/libstackmate.d
-strip ../builds/x86_64-linux-android/release/libstackmate.a
+cd .. && make android
+
+cp ./target/aarch64-linux-android/release/libstackmate.so ../builds/aarch64-linux-android/release/
+cp ./target/x86_64-linux-android/release/libstackmate.so ../builds/x86_64-linux-android/release/
+cp ./target/i686-linux-android/release/libstackmate.so ../builds/i686-linux-android/release/
+cp ./target/armv7-linux-androideabi/release/libstackmate.so ../builds/armv7-linux-androideabi/release/
+# strip ../builds/aarch64-linux-android/release/libstackmate.so
+# strip ../builds/x86_64-linux-android/release/libstackmate.so
 # strip ../builds/i686-linux-android/release/libstackmate.so
 
 zip -r ../builds.zip ../builds
