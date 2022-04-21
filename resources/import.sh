74373efd53082ca112be94e5eb9d7a3e63ddc786
Vishal Menon <vishalmenon.92@gmail.com>
2022-04-22 01:07:38 +0530

    Added 0.8.0 release

--- resources/import.sh
+++ resources/import.sh
@@ -2,9 +2,9 @@
 
 SERVER=debian@scb
 SERVER_DIRECTORY=/home/debian/stackmate-core/builds
-LOCAL_DIRECTORY=../builds
 
-rm -rf $LOCAL_DIRECTORY
-mkdir -p $LOCAL_DIRECTORY
+rm -rf ../builds
 
-scp -r "$SERVER:$SERVER_DIRECTORY" "$LOCAL_DIRECTORY"
+scp -r "$SERVER:$SERVER_DIRECTORY" ../
+
+# tar -czvf releases.tar ../builds
