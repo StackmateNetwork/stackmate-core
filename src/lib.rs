5c50fdca011fef2100ae63661457360b0bbd46e1
Vishal Menon <vishalmenon.92@gmail.com>
2022-05-10 10:05:46 +0530

    Addeed tor start,status,stop to api

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,7 @@ Developed by Stackmate India in 2021.
 //! 7. Bitcoin-core RPC is supported but not advised unless on desktop where a node is connected to locally.
 //! 8. Core RPC (currently) requies node_address to follow the format of 'https://address:port?auth=username:password'.
 //! 9. Outputs of each function are JSON stringified native structs specified as 'FFI Outputs' in under module documentation.
-//! 10. *Use every function in combination with cstring_free to free their output pointers. This will keep things safe.*
+//! 10. *Use every function in combination with cstring_free to free their output pointers. This will keep things safe.* MOST ffi libraries should handle running free() on pointer responses, but cstring_free is there incase you are not sure.
 //!
 //! ## Building a transaction
 //! 1. Build a transaction with a default fixed fee of 1000 sats
@@ -56,7 +56,7 @@ use crate::wallet::recover::RecoveryOption;
 mod network;
 use crate::network::fees;
 use crate::network::height;
-// use crate::network::tor;
+use crate::network::tor;
 
 mod bip392;
 /// Generates a mnemonic phrase of a given length. Defaults to 24 words.
@@ -1167,54 +1167,54 @@ pub unsafe extern "C" fn days_to_blocks(days: *const c_char) -> *mut c_char {
 /// # Safety
 /// - This function is unsafe because it dereferences and a returns raw pointer.
 /// - ENSURE that result is passed into cstring_free(ptr: *mut c_char) after use.
-// #[no_mangle]
-// pub unsafe extern "C" fn tor_start(tmp_path: *mut c_char) -> *mut c_char {
-//     let tmp_path_cstr = CStr::from_ptr(tmp_path);
-//     let tmp_path: &str = match tmp_path_cstr.to_str() {
-//         Ok(string) => string,
-//         Err(_) => "/tmp",
-//     };
+#[no_mangle]
+pub unsafe extern "C" fn tor_start(tmp_path: *mut c_char) -> *mut c_char {
+    let tmp_path_cstr = CStr::from_ptr(tmp_path);
+    let tmp_path: &str = match tmp_path_cstr.to_str() {
+        Ok(string) => string,
+        Err(_) => "/tmp",
+    };
 
-//     let control_key = tor::start(tmp_path);
-//     CString::new(control_key).unwrap().into_raw()
-// }
+    let control_key = tor::start(tmp_path);
+    CString::new(control_key).unwrap().into_raw()
+}
 
 /// Get bootstrap progress from tor daemon. Wait ~1s after calling tor_start() before calling this.
 /// BETA: Careful with this.
 /// # Safety
 /// - This function is unsafe because it dereferences and a returns raw pointer.
 /// - ENSURE that result is passed into cstring_free(ptr: *mut c_char) after use.
-// #[no_mangle]
-// pub unsafe extern "C" fn tor_progress(control_key: *mut c_char) -> *mut c_char {
-//     let control_key_cstr = CStr::from_ptr(control_key);
-//     let control_key: &str = match control_key_cstr.to_str() {
-//         Ok(string) => string,
-//         Err(_) => return S5Error::new(ErrorKind::Input, "Control-Key").c_stringify(),
-//     };
-//     match tor::bootstrap_progress(control_key) {
-//         Ok(result) => CString::new(result.to_string()).unwrap().into_raw(),
-//         Err(e) => e.c_stringify(),
-//     }
-// }
+#[no_mangle]
+pub unsafe extern "C" fn tor_progress(control_key: *mut c_char) -> *mut c_char {
+    let control_key_cstr = CStr::from_ptr(control_key);
+    let control_key: &str = match control_key_cstr.to_str() {
+        Ok(string) => string,
+        Err(_) => return S5Error::new(ErrorKind::Input, "Control-Key").c_stringify(),
+    };
+    match tor::bootstrap_progress(control_key) {
+        Ok(result) => CString::new(result.to_string()).unwrap().into_raw(),
+        Err(e) => e.c_stringify(),
+    }
+}
 
 /// Shutdown tor daemon.
 /// BETA: Careful with this.
 /// # Safety
 /// - This function is unsafe because it dereferences and a returns raw pointer.
 /// - ENSURE that result is passed into cstring_free(ptr: *mut c_char) after use.
-// #[no_mangle]
-// pub unsafe extern "C" fn tor_stop(control_key: *mut c_char) -> *mut c_char {
-//     let control_key_cstr = CStr::from_ptr(control_key);
-//     let control_key: &str = match control_key_cstr.to_str() {
-//         Ok(string) => string,
-//         Err(_) => return S5Error::new(ErrorKind::Input, "Control-Key").c_stringify(),
-//     };
+#[no_mangle]
+pub unsafe extern "C" fn tor_stop(control_key: *mut c_char) -> *mut c_char {
+    let control_key_cstr = CStr::from_ptr(control_key);
+    let control_key: &str = match control_key_cstr.to_str() {
+        Ok(string) => string,
+        Err(_) => return S5Error::new(ErrorKind::Input, "Control-Key").c_stringify(),
+    };
 
-//     match tor::shutdown(control_key) {
-//         Ok(result) => CString::new(result.to_string()).unwrap().into_raw(),
-//         Err(e) => e.c_stringify(),
-//     }
-// }
+    match tor::shutdown(control_key) {
+        Ok(result) => CString::new(result.to_string()).unwrap().into_raw(),
+        Err(e) => e.c_stringify(),
+    }
+}
 
 /// After using any other function, pass the output pointer into cstring_free(ptr: *mut c_char) to clear memory.
 /// ALWAYS use this in combination with any other function.
